820d9a9a9f92aa01ab9ac58de97dc221e6606100
yuz727 <alexzhoulol@gmail.com>
2024-11-08 20:12:41 -0500

    test

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+  public GameObject Player;
+
+  // Update is called once per frame
+  void Update()
+  {
+    // if (Input.GetKeyDown("a"))
+    // {
+
+    // }
+    // if (Input.GetKeyDown("d"))
+    // {
+    // }
+    if (Input.GetKeyDown("space"))
+    {
+      Debug.Log("?");
+    }
+  }
+}
